0faa4a74f23d0a4795972c3965672f1ccf93fe58
Danilo Souza <danilos063@gmail.com>
2018-03-10 21:00:34 -0500

    Implemented Projectile Class Based on UML Sepcifications

--- src/Model/Projectile.java
+++ src/Model/Projectile.java
@@ -1,4 +1,21 @@
 package Model;
 
+import java.awt.Point;
+
 public class Projectile {
+	private int damage;
+	private Point direction;
+	
+	Projectile(int _damage, Point _direction){
+		damage = _damage;
+		direction = _direction;
+	}
+	
+	public Point getDirection() {
+		return direction;
+	}
+	
+	public int getDamage() {
+		return damage;
+	}
 }
