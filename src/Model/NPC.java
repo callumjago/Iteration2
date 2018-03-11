3d68e2914f1162c3d0606631366d3d4361bca169
Danilo Souza <danilos063@gmail.com>
2018-03-10 21:46:17 -0500

    Implemented NPC Based on UML
    getMove() function needs to be finished

--- src/Model/NPC.java
+++ src/Model/NPC.java
@@ -1,4 +1,22 @@
 package Model;
 
 public class NPC extends Entity {
+	private AI ai;
+	private String dialogue;
+	
+	public void setAI(AI _ai) {
+		ai = _ai;
+	}
+	
+	/*public  getMove() {
+		return ai.getMove();
+	}*/
+	
+	public String getDialogue() {
+		return dialogue;
+	}
+	
+	public void setDialogue(String _dialogue) {
+		dialogue = _dialogue;
+	}
 }
