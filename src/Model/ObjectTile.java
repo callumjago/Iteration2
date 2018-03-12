c714e1a022ead9b56eba21597638de573cef63b2
Danilo Souza <danilos063@gmail.com>
2018-03-12 15:17:58 -0400

    Created ObjectTile and Obstacle Class

--- src/Model/ObjectTile.java
+++ src/Model/ObjectTile.java
@@ -1,4 +1,13 @@
 package Model;
 
 public class ObjectTile extends Tile {
+	private GameObject go;
+	
+	ObjectTile(GameObject _go){
+		go = _go;
+	}
+	
+	public GameObject getGameObject() {
+		return go;
+	}
 }
