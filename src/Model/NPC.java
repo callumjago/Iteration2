1af6ba0cc7e8ccf5dcd6a1c9d146a3e380fe4d9e
Danilo Souza <danilos063@gmail.com>
2018-03-12 15:08:18 -0400

    Implemented The NPC class

--- src/Model/NPC.java
+++ src/Model/NPC.java
@@ -3,13 +3,22 @@ package Model;
 public class NPC extends Entity {
 	private AI ai;
 	private String dialogue;
+	private int ExpUponDeath;
+	
+	NPC(int exp){
+		ExpUponDeath = exp;
+	}
+	
+	NPC(){
+		ExpUponDeath = 10;
+	}
 	
 	public void setAI(AI _ai) {
 		ai = _ai;
 	}
 	
-	/*public  getMove() {
-		return ai.getMove();
+	/*public int getMove() {
+		return ai.getMove(); //getMove from ai should return an angle 
 	}*/
 	
 	public String getDialogue() {
@@ -18,5 +27,13 @@ public class NPC extends Entity {
 	
 	public void setDialogue(String _dialogue) {
 		dialogue = _dialogue;
+	}
+	
+	public int getExpUponDeath() {
+		return ExpUponDeath;
+	}
+	
+	public void setExpUponDeath(int exp) {
+		ExpUponDeath = exp;
 	}
 }
