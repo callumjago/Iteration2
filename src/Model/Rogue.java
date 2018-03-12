8cbb4eeb32cc5d99b6a7928e34ce06560da3aee1
greenphantom <daniel.tymecki@gmail.com>
2018-03-12 15:09:40 -0400

    Started PlayerClasses

--- src/Model/Rogue.java
+++ src/Model/Rogue.java
@@ -1,4 +1,23 @@
 package Model;
 
-public class Rogue {
+public class Rogue implements PlayerClass {
+    @Override
+    public Skill getSkill(int SkillIndex) {
+        return null;
+    }
+
+    @Override
+    public void addSkill(Skill newSkill) {
+
+    }
+
+    @Override
+    public Skill[] getTotalSkills() {
+        return new Skill[0];
+    }
+
+    @Override
+    public void LevelUp(int newLvl) {
+
+    }
 }
