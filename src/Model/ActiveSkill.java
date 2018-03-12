d2f6fe9ca1b34bba607ef561228bcd71af5ffac4
greenphantom <daniel.tymecki@gmail.com>
2018-03-12 13:09:45 -0400

    Implemented Stat System
    Done in prepraration for implementing PlayerClass within Player.

---
+++ src/Model/ActiveSkill.java
@@ -0,0 +1,14 @@
+//TODO: Implement after Interaction system is complete
+package Model;
+
+public class ActiveSkill extends Skill {
+    @Override
+    public void ApplySkill() {
+
+    }
+
+    @Override
+    public void RemoveSkill() {
+
+    }
+}
--- src/Model/Skill.java
+++ src/Model/Skill.java
@@ -1,4 +1,48 @@
 package Model;
 
-public class Skill {
+public abstract class Skill {
+
+    private String Name;
+    private String Description;
+    private Level ReqLvl;
+
+    Skill(){
+        Name = "Dummy";
+        Description = "Insert Dummy Here";
+        ReqLvl = new Level();
+    }
+
+    public Skill(String name, String description, Level reqLvl) {
+        Name = name;
+        Description = description;
+        ReqLvl = reqLvl;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public Level getReqLvl() {
+        return ReqLvl;
+    }
+
+    public void setReqLvl(Level reqLvl) {
+        ReqLvl = reqLvl;
+    }
+
+    public abstract void ApplySkill();
+
+    public abstract void RemoveSkill();
 }
