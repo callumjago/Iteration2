0c012e78556808df8eed4fb2b5e211736601eb56
jorg12 <35468317+jorg12@users.noreply.github.com>
2018-03-13 13:23:41 -0400

    Teleport - maybe-

--- src/Model/Teleport.java
+++ src/Model/Teleport.java
@@ -6,23 +6,23 @@ public class Teleport implements AOE {
     private Point location;
     private int mapID;
 
-    private void Teleport (Point location, int MapID){
+    public Teleport (Point location, int MapID){
         this.location = location;
         this.mapID = mapID;
     }
 
-    private void setMapID(int mapID){
+    public void setMapID(int mapID){
         this.mapID = mapID;
     }
-    private int getMapID(){
+    public int getMapID(){
         return mapID;
     }
 
-    private void setLocation(Point location){
+    public void setLocation(Point location){
         this.location = location;
     }
 
-    private Point getLocation(){
+    public Point getLocation(){
         return location;
     }
 }
