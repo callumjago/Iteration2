bd74f514b2078c5376ad56dbd59ec15c6a6f62a4
greenphantom <daniel.tymecki@gmail.com>
2018-03-13 14:22:36 -0400

    Implemented the framework for the IR Handler

--- src/Model/InteractionHandler.java
+++ src/Model/InteractionHandler.java
@@ -1,4 +1,47 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class InteractionHandler {
+
+    public InteractionHandler(){}
+
+    public void generateInteractions(GameState GS){
+        ArrayList<Entity> entities = GS.getEntities();
+         ArrayList<ArrayList<Tile>> tileset = GS.getTileSet();
+        for (int e = 0; e < entities.size(); ++e){
+            Entity ent = entities.get(e);
+            Tile tile = GS.getTile(ent.getPosition());
+            if (tile instanceof ObjectTile){ // Means a generation needs to be implemented
+                if (tile.getTileObjectID() == 2) { // Map Transition
+                    //ent.setInteraction(new HealingIR(ent,tile.getObjectType())
+                }
+                else if (tile.getTileObjectID() == 3) { // Instant Death
+
+                }
+                else if (tile.getTileObjectID() == 4) { // Teleport
+
+                }
+                else if (tile.getTileObjectID() == 5) {  // Healing Effect
+
+                }
+                else if (tile.getTileObjectID() == 6) {  // Damage Effect
+
+                }
+                else if (tile.getTileObjectID() == 7) {  // Exp Effect
+
+                }
+                else if (tile.getTileObjectID() == 8) {  // Item
+
+                }
+                else if (tile.getTileObjectID() == 9) {  // OneShot
+
+                }
+            }
+            else{
+                ent.clearInteracton();
+            }
+        }
+    }
+
 }
