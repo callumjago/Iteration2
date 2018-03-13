70119162c87ea7a153d4b15959afdaa39558df48
Lorenzo Tabares <10tabareslorenzo@gmail.com>
2018-03-13 13:57:20 -0400

    AOE

--- src/Model/AOE.java
+++ src/Model/AOE.java
@@ -1,4 +1,25 @@
 package Model;
 
 public abstract AOE extends GameObject {
+    private int ID;
+    private Image sprite;
+
+    public AOE(int objectID, Image sprite){
+            super(int objectID, Image sprite);
+
+    }
+
+    public AOE(){
+            super();
+
+    }
+    public int getID()
+    {
+        return this.ID
+    }
+    public setID(int id)
+    {
+        this.ID = id
+    }
+
 }
