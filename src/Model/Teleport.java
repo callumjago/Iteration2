2975a046560a8c331edfa4d934119a5ecc885438
jorg12 <35468317+jorg12@users.noreply.github.com>
2018-03-13 13:18:11 -0400

    Teleport -maybe-

--- src/Model/Teleport.java
+++ src/Model/Teleport.java
@@ -1,4 +1,28 @@
 package Model;
 
+import java.awt.*;
+
 public class Teleport implements AOE {
+    private Point location;
+    private int mapID;
+
+    private void Teleport (Point location, int MapID){
+        this.location = location;
+        this.mapID = mapID;
+    }
+
+    private void setMapID(int mapID){
+        this.mapID = mapID;
+    }
+    private int getMapID(){
+        return mapID;
+    }
+
+    private void setLocation(Point location){
+        this.location = location;
+    }
+
+    private Point getLocation(){
+        return location;
+    }
 }
