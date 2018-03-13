68fa695dd36dceb1fc02b37b5c5355b3a0dda274
Danilo Souza <danilos063@gmail.com>
2018-03-13 14:56:16 -0400

    Updated Tile, ObjectTile

--- src/Model/Tile.java
+++ src/Model/Tile.java
@@ -1,20 +1,57 @@
 package Model;
 
-import java.awt.*;
+import java.awt.Point;
 
-public abstract class Tile {
-    private Point position;
-    private int terrainID;
-    private boolean passable;
-
-    public Tile(int terrainID) {
-
-        this.terrainID = terrainID;
-    }
-
-    public int getTerrainID() {
-        return terrainID;
-    }
-
-    abstract int getTileObjectID();
+public class Tile {
+	private Point position;
+	private int terrainID;
+	private boolean passable;
+	
+	public Tile(Point _pos, int _terrainID){
+		position = _pos;
+		terrainID = _terrainID;
+		setPassable();
+	}
+	
+	public Tile(){
+		terrainID = 0;
+		position = new Point(0, 0);
+		setPassable();
+	}
+	
+	public Point getPosition() {
+		return position;
+	}
+	
+	public boolean getPassable() {
+		return passable;
+	}
+	
+	public int getTerrainID() {
+		return terrainID;
+	}
+	
+	public void setPassable() {
+		switch(terrainID) {
+		case 0:
+			passable = true;
+			break;
+			
+		case 1:
+			passable = false;
+			break;
+			
+		case 2:
+			passable = false;
+			break;
+		}
+	}
+	
+	public void setPassable(boolean _passable) {
+		passable = _passable;
+	}
+	
+	public int getTileObjectID() { //Temporary implementation
+		return 0;
+	}
 }
