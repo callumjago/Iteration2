3bb4bbc8a086e7f445b57b9b5657c044b6d95af2
jorg12 <35468317+jorg12@users.noreply.github.com>
2018-03-15 16:41:34 -0400

    template loadgame

--- src/Model/EquipmentCodex.java
+++ src/Model/EquipmentCodex.java
@@ -1,12 +1,5 @@
 package Model;
-
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
-import java.util.Set;
 
 public class EquipmentCodex  extends Codex{
 
