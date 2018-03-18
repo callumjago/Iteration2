7531d39d4a764c59f528009a2aedb13c27fd3f23
greenphantom <daniel.tymecki@gmail.com>
2018-03-17 22:54:32 -0400

    :fire: Arcane Burst Skill now implemented :fish:

---
+++ src/Model/ArcaneBurstSkill.java
@@ -0,0 +1,55 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class ArcaneBurstSkill extends Skill {
+    private Player player;
+    private GameState GS;
+
+    public ArcaneBurstSkill(Player player, GameState GS){
+        super("Arcane Burst", "Blast Arcane energy to damage foes around you!", new Level(5), new SkillLevel(3));
+        this.player = player;
+        this.GS = GS;
+    }
+
+    @Override
+    public void ApplySkill() {
+        int damage = 75;
+        int range = 2;
+        int mpCost = 50;
+        if (getLvl() == 2){
+            damage = 100;
+            mpCost = 60;
+            range = 3;
+        }
+        else if (getLvl() == 3){
+            damage = 150;
+            mpCost = 75;
+            range = 4;
+        }
+        if (player.checkCast(-mpCost)){
+            player.modifyMP(-mpCost);
+            InfluenceRadius n = new InfluenceRadius(player.getPosition(), range);
+            n.extendInfluence();
+            for (int j = 0; j < range; j++){
+                for (int i = 0; i < n.getInfluence().size(); i++) {
+                    GS.addEntity(new Projectile(n.getInfluence().get(i), player.getOrientation().getDegree(), damage, 0,1));
+                }
+                n.extendInfluence();
+            }
+        }
+        else{
+            System.out.println("Not enough MP!");
+        }
+    }
+
+    @Override
+    public void RemoveSkill() {
+        return;
+    }
+
+    @Override
+    void getSpecificStats(ArrayList<String> stats) {
+
+    }
+}
