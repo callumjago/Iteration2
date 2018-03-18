d9676488e2a45896de789d3c3462e8cb3b54045b
callumjago <cjago@ufl.edu>
2018-03-17 21:17:50 -0400

    Started transaction

--- src/Model/Transaction.java
+++ src/Model/Transaction.java
@@ -1,3 +1,15 @@
 package Model;
 
+public class Transaction implements Interaction {
+    private NPC npc;
+    private Player player;
+    public Transaction(NPC npc, Player player) {
+        this.npc = npc;
+        this.player = player;
+    }
+
+    @Override
+    public void applyEffect() {
+
+    }
 }
